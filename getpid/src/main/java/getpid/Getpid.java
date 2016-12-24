19625ab7dae9e6c9b558f072fce1d6cbd1c16132
Jerome Robert <jeromerobert@gmx.com>
2016-12-24 12:11:04 +0100

    Do as if pid_t was a struct
    It aims at illustrating jnr/jnr-ffi#14

--- getpid/src/main/java/getpid/Getpid.java
+++ getpid/src/main/java/getpid/Getpid.java
@@ -1,20 +1,31 @@
 package getpid;
 
 import jnr.ffi.*;
-import jnr.ffi.types.pid_t;
+import jnr.ffi.Runtime;
 
 /**
  * Gets the process ID of the current process, and that of its parent.
  */
 public class Getpid {
+    // Imagin that pid_t was a structure, not an int
+    public static class Pid extends Struct {
+        public Pid(Runtime runtime) {
+            super(runtime);
+        }
+        public Signed32 value;
+    }
     public interface LibC  {
-        public @pid_t long getpid();
-        public @pid_t long getppid();
+        // Now there is a bug here because getpid return a pid_t, not a pid_t*
+        Pid getpid();
+        Pid getppid();
+        // May be there should be an anotation for that:
+        // @OutValue Pid getpid();
+        // @OutValue Pid getppid();
     }
 
     public static void main(String[] args) {
         LibC libc = LibraryLoader.create(LibC.class).load("c");
 
-        System.out.println("pid=" + libc.getpid() + " parent pid=" + libc.getppid());
+        System.out.println("pid=" + libc.getpid().value + " parent pid=" + libc.getppid().value);
     }
 }
